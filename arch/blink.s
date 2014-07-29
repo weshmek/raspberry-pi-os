f9f0539b007c016e69105384be33f6c0c881ea0b
weshmek <weshmek@github.com>
2014-07-29 19:28:29 -0400

    Put all the handwritten assembly into arch folder

---
+++ arch/blink.s
@@ -0,0 +1,33 @@
+#include <main.h>
+.section .text
+.globl blink
+blink:
+ldr r1 ,= 0x20200000
+mov r2,#1
+lsl r2,#18
+str r2, [r1, #4]
+
+mov r2, #1
+lsl r2, #16
+str r2, [r1, #40]
+
+mov r3, #0x3F0000
+wait1$:
+sub r3, #1
+cmp r3, #0
+bne wait1$
+
+str r2, [r1, #28]
+
+mov r3, #0x3F0000
+wait2$:
+sub r3, #1
+cmp r3, #0
+bne wait2$
+
+sub r0, #1
+cmp r0, #0
+bne blink
+bx lr
+
+b loop$
---
+++ arch/main.s
@@ -0,0 +1,16 @@
+#include <kmain.h>
+
+.section .init
+.globl _start
+_start:
+@ldr sp, .L0
+mov sp,#0x80000 @Stack starts here. Where the hell is the best place to put it? 
+bl kmain
+
+
+.globl loop$
+.section .text
+loop$:
+b loop$
+.L0:
+	.word 0xEFFFFFFF
